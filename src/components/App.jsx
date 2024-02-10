f7c326ae8b87c258dc1986d92172a16b2346d83c
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-02-10 16:46:22 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { useState } from "react";
+import TodoList from "./TodoList";
+import InputField from "./InputField";
+import './App.css';
+import { addTodo } from "store/todoSlice";
+import { useDispatch } from "react-redux";
+
+
+function App() {
+
+const [text, setText]= useState('');
+
+const dispatch = useDispatch();
+const addTask =()=> {dispatch(addTodo({text}))
+setText('')};
+
+
+
+return(
+  <div className="App">
+ 
+  <InputField text={text} handelInput={setText} handelSubmit={addTask}/>
+  <TodoList/>
+  </div>
+);
+}
+
+export default App;
---
+++ src/components/InputField.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InputField = ({text, handelInput, handelSubmit}) => {
+  return (
+    <label>
+    <input value={text} onChange={(e)=>handelInput(e.target.value)}/>
+    <button onClick={handelSubmit}>Add Todo</button>
+  </label>
+  )
+}
+
+export default InputField
